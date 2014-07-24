dd63b3b5e9a43f53b8efbb09786fbf099ebeb407
mshock <shockleyme@gmail.com>
2014-07-23 20:40:34 -0500

    hide from dock, switcher and menubar

--- bar/AppDelegate.m
+++ bar/AppDelegate.m
@@ -25,14 +25,21 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    // format window into bar
+    
+    // the proper level under main mac menu
     self.window.level = NSMainMenuWindowLevel;
+    // remove window header
     [self.window setStyleMask:NSBorderlessWindowMask];
+    // set bgcolor
     NSColor *menuColor = [NSColor colorWithCalibratedRed:85 green:85 blue:85 alpha: 0];
     self.window.backgroundColor = menuColor;
+    // retrieve screen size, set bar size
     NSRect screenDims = [[NSScreen mainScreen] frame];
     //NSLog(@"%f x %f", screenDims.size.height, screenDims.size.width);
     NSRect windowFrame = CGRectMake(0.0, screenDims.size.height-25, screenDims.size.width, 25);
     [self.window setFrame:windowFrame display:YES];
+    
 }
 
 @end
